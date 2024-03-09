5fb12d67ebeff5aa78ca9f4773f8dee8707e4235
Ganesh <ganeshking9047@gmail.com>
2024-03-09 10:39:16 +0530

    word scrambler with login

--- log.php
+++ log.php
@@ -35,27 +35,3 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="style2.css">
-</head>
-<body class="body">
-    <div class="login">
-        <h1>Login</h1>
-        <form method="POST">
-            <label>Email</label>
-            <input type="email" name="email" required>
-            <label>Password</label>
-            <input type="password" name="pass" required>
-            <input type="submit" name="" value="Submit"> 
-        </form>
-        <p class="para">Not have an account?<a href="signup.php">Sign Up here</a></p>
-    </div>
-    
-</body>
-</html>
